22960ee0167c46651e4e573cfd5a007c05cb01f3
Robert Rand <rrand@seas.upenn.edu>
2017-04-09 23:57:56 -0400

    apply functions for non-unitaries

--- Denotation.v
+++ Denotation.v
@@ -6,6 +6,7 @@ Require Import Examples.
 Require Import List.
 Require Import Arith.
 Require Import Quantum.
+Require Import Omega.
 Import ListNotations.
 
 Class Denote source target :=
@@ -129,9 +130,6 @@ Eval compute in (denote_unitary CNOT 3%nat 2%nat).
 Eval compute in (denote_unitary CNOT 3%nat 3%nat).
 *)
 
-Require Import Coq.Vectors.Vector.
-
-Definition LList := t.
 
 (*
 Eval simpl in (from_HOAS_Box hadamard_measure).
@@ -139,28 +137,92 @@ Eval compute in (from_HOAS_Box hadamard_measure).
 Eval vm_compute in (from_HOAS_Box hadamard_measure).
 *)
 
-Require Import Omega.
-
-(* Right now, assuming a unitary is applied to some larger density matrix *)
-
-Program Fixpoint swap_list {m n} (v : LList {i : nat | i < n} m) : Square (2^n) :=
-  match v with 
-  | nil => Id 1
-  | cons i _ v' => (swap_to_0 n i) × ((Id 2) ⊗ (swap_list v'))
+Program Fixpoint zip_to (m n: nat) (l : list (sig (fun i => i < n))) :
+  list ((sig (fun i => i < n)) * (sig (fun i => i < n))) :=
+  match l with
+  | nil => nil 
+  | x :: xs => match m <? n with 
+              | true => (m, x) :: zip_to (S m) n xs
+              | false => nil
+              end
   end.
+Next Obligation. apply Nat.ltb_lt. auto. Defined. 
 
-Definition pad {m} n (pf: m <= n) (A : Square (2^m)) : Square (2^n) :=
-  (A ⊗ Id (2^n / 2^m)).
+Program Fixpoint swap_list_aux 
+  (n : nat) (l : list ((sig (fun i => i < n)) * (sig (fun i => i < n)))) : Square  (2^n) :=
+  match l with
+  | nil => Id (2^n)
+  | cons (a,b) xs => swap_two n a b × swap_list_aux n xs
+  end. 
 
-Definition apply {m n} (U : Square (2^m)) (ρ : Density (2^n)) (l : LList {i : nat | i < n} m) {pf : m <= n} : Density (2^n) := 
-  let S := swap_list l in 
-  let SU := S × (pad n pf U) × S† in  
-  SU × ρ × SU†.
-   
-(* Or?
-  let S := swap_list l in 
-  S × (pad n pf U)  × S† × ρ × S† × (pad n pf U)† × S. (* Can we remove Ss? *)
+Definition swap_list (n : nat) (l : list (sig (fun i => i < n))) :=
+  swap_list_aux n (zip_to 0 n l). 
+
+Program Definition my_one : {i : nat | i < 2} := S O.
+Lemma swap_list_swap : swap_list 2 [my_one] = swap.
+Proof.
+  simpl.
+  unfold swap_list, swap_list_aux.
+  simpl.
+  rewrite Mmult_1_r.
+  apply swap_two_base.
+  (* .. and we're done here *)
+Admitted.
+
+(*
+
+(*
+Require Import Coq.Vectors.Vector.
+
+Definition LList := t.
 *)
+
+
+Program Fixpoint swap_list {m n} (v : LList (sig (fun i => i < n)) m) : Square (2^n) :=
+  match v with 
+  | nil => Id (2^(n-m))
+  | cons i _ v' => (swap_to_0 n i) × ((Id 2) ⊗ (@swap_list (m-1) (n-1) v'))
+  end.
+Next Obligation. rewrite Nat.sub_0_r. reflexivity. Defined.
+Next Obligation. rewrite Nat.sub_0_r. reflexivity. Defined.
+Next Obligation. rewrite plus_0_r.  reflexivity. Defined.
+Next Obligation. rewrite Nat.sub_0_r. reflexivity. Defined.
+*)
+
+Local Obligation Tactic := program_simpl; unify_pows_two; try omega.
+
+Program Definition pad {m} n (pf: m <= n) (A : Square (2^m)) : Square (2^n) :=
+  (A ⊗ Id (2^ (n - m))).
+
+Definition apply {m n} (U : Square (2^m)) (ρ : Density (2^n)) 
+  (l : list (sig (fun i => i < n))) {pf : m <= n} : Density (2^n) := 
+  let S := swap_list n l in 
+  let SU := S × (pad n pf U) × S† in  
+  super SU ρ.
+
+(*
+Program Definition pad_k0 n : Matrix (2^(S n)) (2^n) := (|0⟩ ⊗ Id (2^n)).
+Program Definition pad_k1 n : Matrix (2^(S n)) (2^n) := (|1⟩ ⊗ Id (2^n)).
+Program Definition pad_b0 n : Matrix (2^n) (2^(S n)) := (⟨0| ⊗ Id (2^n)).
+Program Definition pad_b1 n : Matrix (2^n) (2^(S n)) := (⟨1| ⊗ Id (2^n)).
+*)
+
+Program Definition apply_new0 {n} (ρ : Density (2^n)) (k : sig (fun i => i < n)) : 
+  Square (2^(n+1)) := super (|0⟩ ⊗ Id (2^n)) ρ.
+
+Program Definition apply_new1 {n} (ρ : Density (2^n)) (k : sig (fun i => i < n)) : 
+  Square (2^(n+1)) := super (|1⟩ ⊗ Id (2^n)) ρ.
+
+Program Definition apply_discard {n} (ρ : Density (2^n)) (k : sig (fun i => i < n)) : 
+  Square (2^(n-1)) := 
+  let S := @swap_two n 0 k _ _ in 
+  super ((⟨0| ⊗ Id (2^(n-1))) × S) ρ .+ super ((⟨1| ⊗ Id (2^(n-1))) × S) ρ.
+
+Program Definition apply_meas {n} (ρ : Density (2^n)) (k : sig (fun i => i < n)) : 
+  Square (2^n) := 
+  let S := @swap_two n 0 k _ _ in 
+  super (S† × (|0⟩⟨0| ⊗ Id (2^(n-1))) × S) ρ .+ 
+  super (S† × (|1⟩⟨1| ⊗ Id (2^(n-1))) × S) ρ.
 
 
 
