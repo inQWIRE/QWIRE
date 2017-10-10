faa054df3f7d39241889d48eff54d26f513c6ea8
fredldh <fredldh@gmail.com>
2017-10-09 23:53:45 -0400

    Debugged the coin_flip example.

--- QASMExamples.v
+++ QASMExamples.v
@@ -63,12 +63,13 @@ Definition gname : nat -> id := fun x => String (ascii_of_nat 103) (string_of_na
 Definition Hname : id := "gH"%string.
 
 (* Hadamard gate *)
-Example h : program := [s_gatedecl (gname 1) (Some [qname 1]) ([])
+Example h_decl : program := [s_gatedecl Hname (None) ([qname 1])
         ([g_uop (u_U [e_binop e_pi div (e_real 2); e_real 0; e_pi] (a_id (qname 1)))])].
+Print h_decl.
 
 (* coin_flip *)
-Example coin_flip : program := [s_decl (creg (cname 1) 1); s_decl (qreg (qname 1) 1);
-                               s_gatedecl (gname 1) (Some [qname 1]) ([])
-               ([g_uop (u_U [e_binop e_pi div (e_real 2); e_real 0; e_pi] (a_id (qname 1)))]);
-                               s_qop (q_uop (u_app (gname 1) ([a_id (qname 1)])));
+Example coin_flip : program := app h_decl
+                               [s_decl (creg (cname 1) 1); s_decl (qreg (qname 1) 1);
+                               s_qop (q_uop (u_app Hname ([a_id (qname 1)])));
                                s_qop (q_meas (a_id (qname 1)) (a_id (cname 1)))].
+Eval compute in coin_flip.
