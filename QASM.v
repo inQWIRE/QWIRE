39eb379421c38549a24ecf6965765fd8fee4c827
fredldh <fredldh@gmail.com>
2017-11-23 04:27:06 -0500

    QASM.v - Implemented trans function from min circuit to qasm (todo : 1. bit_control, 2. bit_lift, 3. trans_exp). QASMPrinter.v - Added examples (from min circuit to qasm).

--- QASM.v
+++ QASM.v
@@ -1,6 +1,5 @@
 Require Import Reals.
 Require Import String.
-Require Import FlatCircuits.
 
 (* QASM.v - representation of QASM circuits *)
 
@@ -80,18 +79,34 @@ Inductive statement : Set :=
 Definition program := list statement.
 
 (** Convert from Minimal Circuits to QASM **)
+
+(* [Min Circuit] to [QASM] translation procedure 
+   1. Transform [Unitary] gates into a sequence of universal gates (ROT3 and CNOT).
+      - See [min_circuit_translation_helper], [min_circuit_merge], 
+            [unitary_gate_translation], [transpose_unitary_gate_circuit], 
+            [append_gate_last], and [control_unitary_gate_circuit] functions.
+   2. Translate the circuit into [QASM] program
+      - See [trans], [trans'], [trans_exp], [pat_to_anylist], [meta_if], 
+            [meta_if_true], and [meta_if_flase] functions.
+*)
+
 Require Import HOASCircuits.
 Require Import HOASExamples.
 Require Import FlatCircuits.
 Require Import Contexts.
-Require Import List.
 Require Import Arith.
 Require Import Reals.
+Require Import List.
 
 Open Scope circ_scope.
 Open Scope R_scope.
+Import ListNotations.
+Require Import Notations.
 
-(*
+(* Universal gate representation for controlled universal gates
+
+Universal gates - U(theta, phi, lambda) : rotation, cx : CNOT
+
 // 1-parameter 0-pulse single qubit gate
 gate u1(lambda) q { U(0,0,lambda) q; }
 // controlled-U
@@ -220,7 +235,8 @@ Fixpoint unitary_gate_translation {W} (u : Unitary W) (p po : Pat) : Min_Circuit
         control_unitary_gate_circuit p1 (unitary_gate_translation u' p2 po)
       | _ => unitary_gate_translation u' p po
       end
-    | bit_ctrl u' => (min_output po)
+    | bit_ctrl u' => (* Todo : temporarily filled with [min_output po] *)
+      (min_output po)
     | transpose u' => transpose_unitary_gate_circuit (unitary_gate_translation u' p po)
   end.
 
@@ -266,5 +282,196 @@ Eval simpl in (match (hoas_to_min_box bob (Bit ⊗ Bit ⊗ Qubit)) with
 Eval simpl in (match (hoas_to_min_box teleport Qubit) with
                | min_box W C => min_circuit_translation_helper C
                end).
+
+
+(* Translate [min circuit] to [QASM] *)
+
+(** Naming functions for qreg, creg, and bits **)
+Require Import Ascii.
+(* Nat to string converter *)
+Definition natToDigit (n : nat) : ascii :=
+  match n with
+  | 0 => "0" | 1 => "1" | 2 => "2" | 3 => "3" | 4 => "4" | 5 => "5"
+  | 6 => "6" | 7 => "7" | 8 => "8" | _ => "9"
+  end.
+Fixpoint writeNatAux (time n : nat) (acc : string) : string :=
+  let acc' := String (natToDigit (n mod 10)) acc in
+  match time with
+    | 0 => acc'
+    | S time' =>
+      match (n / 10)%nat with
+        | 0 => acc'
+        | n' => writeNatAux time' n' acc'
+      end
+  end.
+Definition writeNat (n : nat) : string :=
+  writeNatAux n n "".
+(* naming function for qregs *)
+Definition qname : nat -> id := fun x => String (ascii_of_nat 113) (writeNat x).
+(* naming function for cregs *)
+Definition cname : nat -> id := fun x => String (ascii_of_nat 99) (writeNat x).
+(* name of the [creg array] used for branching *)
+Definition bname : id := "bits"%string.
+
+Fixpoint pow2 (n : nat) : nat :=
+  match n with
+  | 0 => 1
+  | S n' => 2 * (pow2 n')
+  end.
+
+(* meta_if : return [QASM] code equivalent to [if (bits[bn] == 1) then P1 else P2]  *)
+Fixpoint meta_if_true (p1 : program) (bn : nat) : program :=
+  match p1 with
+  | [] => []
+  | h :: p1' =>
+    (match h with
+     | s_if bname n q =>
+       (s_if bname n q)
+     | s_qop q =>
+       (s_if bname 0 q)
+     | _ => h
+     end) :: (meta_if_true p1' bn)
+  end.
+Fixpoint meta_if_false (p2 : program) (bn : nat) : program :=
+  match p2 with
+  | [] => []
+  | h :: p2' =>
+    (match h with
+     | s_if bname n q =>
+       (s_if bname (n + (pow2 bn)) q)
+     | s_qop q =>
+       (s_if bname (pow2 bn) q)
+     | _ => h
+     end) :: (meta_if_false p2' bn)
+  end.
+Definition meta_if (p1 p2 : program) (bn : nat) : program :=
+  (meta_if_true p1 bn) ++ (meta_if_false p2 bn).
+
+(* trans c li n m : pair p d
+
+   1. Description : A transition function from [min circuit] to [QASM] program.
+   2. Input : 
+     - c  : min circuit
+     - li : context of min circuit (list of variable corresponding to its index)
+     - n  : number of [qreg] or [creg] declared = argument for new variable name
+            ([meas] gate operation creates [creg] variable of the same name with [qreg])
+     - m  : size of [creg] array [bits] for branchgin
+   3. Output :
+     - p  : [QASM] program corresponding to c
+     - d  : depth of branching (size of [creg] array [bits] used in p)
+
+ *)
+Fixpoint pat_to_anylist (li : list Var) (p : Pat) : anylist :=
+  match p with
+  | unit => []
+  | qubit x => [a_id (qname (nth x li 0%nat))]
+  | bit x => [a_id (cname (nth x li 0%nat))]
+  | pair p1 p2 => (pat_to_anylist li p1) ++ (pat_to_anylist li p2)
+  end.
+
+(* trans_exp : translate real numbers to [exp] *)
+Definition trans_exp (r : R) : exp := (* Todo : temporarily filled with [e_pi] *)
+  e_pi.
+
+Fixpoint trans' (c : Min_Circuit) (li : list Var) (n m : nat) :=
+  match c with
+  | min_output p    => ([s_output (pat_to_anylist li p)], 0%nat)
+  | min_gate g p c' =>
+    match g with
+    | U u     =>
+      let (p', d') := (trans' c' li n m) in
+      (((match u with
+         | CNOT =>
+           (match p with
+            | pair (qubit x1) (qubit x2) =>
+              let qx1 := (nth x1 li 0%nat) in
+              let qx2 := (nth x2 li 0%nat) in
+              let a1 := (a_id (qname qx1)) in
+              let a2 := (a_id (qname qx2)) in
+              [s_qop (q_uop (u_CX a1 a2))]
+            | _ => []
+            end) 
+         | ROT3 th ph la =>
+           (match p with
+            | qubit x =>
+              let qx := (nth x li 0%nat) in
+              let a := (a_id (qname qx)) in
+              [s_qop (q_uop (u_U [(trans_exp th); (trans_exp ph); (trans_exp la)] a))]
+            | _ => []
+            end)
+         | _ => []
+         end) ++ p'), d')
+    | init0   =>
+      let (p', d') := (trans' c' (li ++ [n]) (S n) m) in
+      (([s_decl (qreg (qname n) 1)] ++ p'), d')
+    | init1   =>
+      let (p', d') := (trans' c' (li ++ [n]) (S n) m) in
+      (([s_decl (qreg (qname n) 1);
+           s_qop (q_uop (u_U [e_pi; e_nat 0; e_pi] (a_id (qname n))))] ++ p'), d')
+    | new0    =>
+      let (p', d') := (trans' c' (li ++ [n]) (S n) m) in
+      (([s_decl (creg (cname n) 1)] ++ p'), d')
+    | new1    =>
+      let (p', d') := (trans' c' (li ++ [S n]) (S (S n)) m) in
+      (([s_decl (creg (cname (S n)) 1); s_decl (qreg (qname n) 1);
+           s_qop (q_uop (u_U [e_pi; e_nat 0; e_pi] (a_id (qname n))));
+           s_qop (q_meas (a_id (qname n)) (a_id (cname (S n))))]
+          ++ p'), d')
+    | meas    =>
+      let (p', d') := (trans' c' li n m) in
+      ((match p with
+        | qubit x =>
+          let a := (nth x li 0%nat) in
+          ([s_decl (creg (cname a) 1);
+              s_qop (q_meas (a_id (qname a)) (a_id (cname a)))]
+             ++ p')
+        | _ => []
+        end), d')
+    | discard =>
+      match p with
+      | bit x =>
+        let li' := List.remove Nat.eq_dec x li in
+        (trans' c' li' n m)
+      | _ => ([], 0%nat)
+      end
+    end
+  | min_lift p f =>
+    match p with
+    | bit x => ([(* Todo *)], 0%nat)
+    | qubit x =>
+      let li' := List.remove Nat.eq_dec x li in
+      let (p1, d1) := (trans' (f true) li' n m) in
+      let (p2, d2) := (trans' (f false) li' n m) in
+      let bn := (Nat.max d1 d2) in
+      let a := (nth x li 0%nat) in
+      (([s_decl (creg (cname a) 1);
+           s_qop (q_meas (a_id (qname a)) (a_id (cname a)))]
+          ++ (meta_if p1 p2 bn)), (S bn))
+    | _ => ([], 0%nat)
+    end
+  end.
+Fixpoint trans (c : Min_Circuit) (w : WType) : program :=
+  let (p, n) := fresh_pat w 0 in
+  let li := pat_to_list p in
+  let (qasm, d) := (trans' c li n 0%nat) in
+  (match d with
+   | 0 => qasm
+   | S _ =>
+     [s_decl (creg bname d)] ++ qasm
+   end).
+
+(* Min Circuit Translation Examples *)
+Eval simpl in trans (match (hoas_to_min_box bell00 One) with
+                     | min_box W C => min_circuit_translation_helper C
+                     end) One.
+Eval simpl in trans (match (hoas_to_min_box alice (Qubit ⊗ Qubit)) with
+                     | min_box W C => min_circuit_translation_helper C
+                     end) (Qubit ⊗ Qubit).
+Eval simpl in trans (match (hoas_to_min_box bob (Bit ⊗ Bit ⊗ Qubit)) with
+                     | min_box W C => min_circuit_translation_helper C
+                     end) (Bit ⊗ Bit ⊗ Qubit).
+Eval simpl in trans (match (hoas_to_min_box teleport Qubit) with
+                     | min_box W C => min_circuit_translation_helper C
+                     end) Qubit.
 
 Close Scope circ_scope.
--- QASMPrinter.v
+++ QASMPrinter.v
@@ -1,8 +1,10 @@
 Require Import String.
-Require Import List.
 Require Import Ascii.
+Require Import Reals.
 Require Import QASM.
-Require Import QASMFunctionsToString.
+Require Import List.
+
+Import ListNotations.
 
 (* Print function from program to QASM code *)
 Definition newline := String (Ascii.ascii_of_N 10) EmptyString.
@@ -27,7 +29,7 @@ Definition print_unaryop (u : unaryop) : string :=
 Fixpoint print_exp (e : exp) : string :=
   match e with
   | e_real r => "" (* To implement *)
-  | e_nat n => string_of_nat n
+  | e_nat n => writeNat n
   | e_pi => "pi"
   | e_id name => name
   | e_binop e1 b e2 => " ( " ++ print_exp e1 ++ " " ++ print_binop b
@@ -38,25 +40,25 @@ Fixpoint print_exp (e : exp) : string :=
 Definition print_argument (a : argument) : string :=
   match a with
   | a_id n => n
-  | a_idx n i => n ++ " [ " ++ string_of_nat i ++ " ] "
+  | a_idx n i => n ++ " [ " ++ writeNat i ++ " ] "
   end.
 
 Fixpoint print_idlist (args : idlist) : string :=
   match args with
-  | nil => ""
-  | h :: nil => h
+  | [] => ""
+  | h :: [] => h
   | h :: t => h ++ ", " ++ print_idlist t
   end.
 Fixpoint print_explist (exps : explist) : string :=
   match exps with
-  | nil => ""
-  | h :: nil => print_exp h
+  | [] => ""
+  | h :: [] => print_exp h
   | h :: t => print_exp h ++ ", " ++ print_explist t
   end.
 Fixpoint print_anylist (args : anylist) : string :=
   match args with
-  | nil => ""
-  | h :: nil => print_argument h
+  | [] => ""
+  | h :: [] => print_argument h
   | h :: t => print_argument h ++ ", " ++ print_anylist t
   end.
 
@@ -88,15 +90,15 @@ Definition print_gop (op : gop) : string :=
   end.
 Fixpoint print_goplist (gops : goplist) : string :=
   match gops with
-  | nil => ""
-  | h :: nil => print_gop h
+  | [] => ""
+  | h :: [] => print_gop h
   | h :: t => print_gop h ++ print_goplist t
   end.
 
 Definition print_decl (d : decl) : string :=
   match d with
-  | qreg name size => "qreg " ++ name ++ " [ " ++ string_of_nat size ++ " ] ;" ++ newline
-  | creg name size => "creg " ++ name ++ " [ " ++ string_of_nat size ++ " ] ;" ++ newline
+  | qreg name size => "qreg " ++ name ++ " [ " ++ writeNat size ++ " ] ;" ++ newline
+  | creg name size => "creg " ++ name ++ " [ " ++ writeNat size ++ " ] ;" ++ newline
   end.
 Definition print_gatedecl (name:id) (args:option idlist) (names:idlist) (body:goplist)
   : string := "gate " ++ name ++ " ( " ++ print_args args ++ " ) " ++ print_idlist names
@@ -111,12 +113,60 @@ Fixpoint print_statement (s : statement) : string :=
   | s_opaque name args names => "opaque " ++ name ++ " ( " ++ print_args args ++ " ) "
                                           ++ print_idlist names ++ " ;" ++ newline
   | s_qop q => print_qop q
-  | s_if x n q => "if ( " ++ x ++ " == " ++ string_of_nat n ++ " ) " ++ print_qop q
+  | s_if x n q => "if ( " ++ x ++ " == " ++ writeNat n ++ " ) " ++ print_qop q
   | s_barrier args => "barrier " ++ print_anylist args ++ " ;" ++ newline
+  | s_output args => ""
   end.
 
 Fixpoint printer (p : program) : string :=
   match p with
-  | nil => ""
+  | [] => ""
   | st :: p' => print_statement st ++ printer p'
   end.
+
+(* Examples : [Min Circuit] to [QASM] *)
+Require Import HOASCircuits.
+Require Import HOASExamples.
+Require Import FlatCircuits.
+Require Import Contexts.
+Open Scope circ_scope.
+Definition bell00_t1 := Eval simpl in (match (hoas_to_min_box bell00 One) with
+                                       | min_box W C => min_circuit_translation_helper C
+                                       end).
+Definition alice_t1 := Eval simpl in (match (hoas_to_min_box alice (Qubit ⊗ Qubit)) with
+                                      | min_box W C => min_circuit_translation_helper C
+                                      end).
+Definition bob_t1 := Eval simpl in (match (hoas_to_min_box bob (Bit ⊗ Bit ⊗ Qubit)) with
+                                    | min_box W C => min_circuit_translation_helper C
+                                    end).
+Definition teleport_t1 := Eval simpl in (match (hoas_to_min_box teleport Qubit) with
+                                         | min_box W C => min_circuit_translation_helper C
+                                         end).
+
+Definition bell00_t2 := Eval simpl in trans bell00_t1 One.
+Definition alice_t2 := Eval simpl in trans alice_t1 (Qubit ⊗ Qubit).
+Definition bob_t2 := Eval simpl in trans bob_t1 (Bit ⊗ Bit ⊗ Qubit).
+Definition teleport_t2 := Eval simpl in trans teleport_t1 Qubit.
+
+Definition bell00_t3 := Eval compute in printer bell00_t2.
+Definition alice_t3 := Eval compute in printer alice_t2.
+Definition bob_t3 := Eval compute in printer bob_t2.
+Definition teleport_t3 := Eval compute in printer teleport_t2.
+
+Print bell00_t1.
+Print bell00_t2.
+Print bell00_t3.
+
+Print alice_t1.
+Print alice_t2.
+Print alice_t3.
+
+Print bob_t1.
+Print bob_t2.
+Print bob_t3.
+
+Print teleport_t1.
+Print teleport_t2.
+Print teleport_t3.
+
+Close Scope circ_scope.
