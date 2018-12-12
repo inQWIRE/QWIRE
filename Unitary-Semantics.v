e997bf60379a782c2e4f7a2b918bc47e108a4726
Robert Rand <rxtreme@gmail.com>
2018-12-12 18:03:29 -0500

    second attempt, unitary in prop, not used in computation

--- Unitary-Semantics.v
+++ Unitary-Semantics.v
@@ -4,24 +4,24 @@ Open Scope matrix_scope.
 
 (* Propositional version (in Set). Could also just have a unitary circuit type and a 
    (trivial) coercion from circuit. *)
-Inductive Unitary_Circuit {W} : Circuit W -> Set :=
+Inductive Unitary_Circuit {W} : Circuit W -> Prop :=
 | u_output : forall p, Unitary_Circuit (output p)
 | u_gate : forall W' c (u : Unitary W') p,
     (forall p', Unitary_Circuit (c p')) ->
     Unitary_Circuit (gate (U u) p c).
 
-Definition Unitary_Box {W} (b : Box W W) : Set :=
+Definition Unitary_Box {W} (b : Box W W) : Prop :=
   match b with
   | box c => forall p, (Unitary_Circuit (c p))
   end.
 
-Inductive Unitary_DB_Circuit {W} : DeBruijn_Circuit W -> Set :=
+Inductive Unitary_DB_Circuit {W} : DeBruijn_Circuit W -> Prop :=
 | u_db_output : forall p, Unitary_DB_Circuit (db_output p)
 | u_db_gate : forall W' c (u : Unitary W') p,
     Unitary_DB_Circuit c ->
     Unitary_DB_Circuit (db_gate (U u) p c).
 
-Definition Unitary_DB_Box {W} (b : DeBruijn_Box W W) : Set :=
+Definition Unitary_DB_Box {W} (b : DeBruijn_Box W W) : Prop :=
   match b with
   | db_box _ c => Unitary_DB_Circuit c
   end.
@@ -33,44 +33,20 @@ Definition apply_unitary {W} (n : nat) (U : Unitary W) (li : list nat) : Square 
   | _     => denote_ctrls n U li
   end.
 
-(*
-apply_U = 
-fun (W : WType) (n : nat) (U : Unitary W) (l : list nat) =>
-match W with
-| Qubit => apply_to_first (apply_qubit_unitary (⟦ U ⟧)) l
-| _ => super (denote_ctrls n U l)
-end
-     : forall (W : WType) (n : nat), Unitary W -> list nat -> Superoperator (2 ^ n) (2 ^ n)
-*)
-
-Fixpoint denote_u_db_circuit {W} n (c : DeBruijn_Circuit W) (pf : Unitary_DB_Circuit c)
-                         : Square (2^n) :=
-  match pf with
-  | u_db_output p => pad n (⟦p⟧)
-  | u_db_gate W' c u p pf' => (denote_u_db_circuit n c pf') × (apply_unitary n u (pat_to_list p))
-  end.
-
-(*
-Fixpoint denote_u_db_circuit {w} n (c : DeBruijn_Circuit w) (pf : Unitary_DB_Circuit c)
-                         : Square (2^n) :=
+Fixpoint denote_u_db_circuit {W} n (c : DeBruijn_Circuit W) : Square (2^n) :=
   match c with
-  | db_output p    => pad n (⟦p⟧)
-  | db_gate g p c' => match g with
-                              | U u => (denote_u_db_circuit n c') ×
-                                      (apply_unitary n u (pat_to_list p))
-                              | _ => IDProp
-                              end
-  | _              => IDProp
+  | db_output p => pad n (⟦p⟧)
+  | db_gate g p c  => match g with
+                     | U u => (denote_u_db_circuit n c) × (apply_unitary n u (pat_to_list p))
+                     | _   => dummy_mat
+                     end
+  | _   => dummy_mat
   end.
-*)
 
-Print Unitary_DB_Box.
-
-Definition denote_db_box {W} (b : DeBruijn_Box W W) (pf : Unitary_DB_Box b) : Square (2^⟦W⟧).
-  unfold Unitary_DB_Box in pf.
-  destruct b.
-  exact (denote_u_db_circuit (⟦W⟧) d pf).
-Defined.
+Definition denote_u_db_box {W} (c : DeBruijn_Box W W) : Square (2^⟦W⟧) :=
+  match c with
+  | db_box _ c' => denote_u_db_circuit (⟦W⟧) c' 
+  end.
 
 Lemma unitary_to_db : forall W Γ (c : Circuit W) , Unitary_Circuit c -> Unitary_DB_Circuit (hoas_to_db Γ c).
 Proof.
@@ -83,7 +59,7 @@ Proof.
     dependent destruction U.
     constructor. 
     apply H.
-    apply u0.
+    apply H0.
   - inversion U.
 Qed.
 
@@ -97,3 +73,38 @@ Proof.
   apply U.
 Qed.
 
+(*
+denote_circuit
+
+Definition denote_unitary_circuit {W} 
+*)
+
+Definition denote_unitary_box {W} (c : Box W W) :=
+  denote_u_db_box (hoas_to_db_box c).
+
+Lemma denote_unitary_box_eq : forall W safe (c : Box W W) ρ,
+    Unitary_Box c ->
+    denote_box safe c ρ = denote_unitary_box c × ρ × (denote_unitary_box c)†.
+Proof.
+  intros W safe [c] ρ pf.
+  simpl in pf.
+  unfold denote_unitary_box, denote_box.
+  unfold denote_db_box.
+  unfold hoas_to_db_box.
+  destruct (add_fresh W []) as [p Γ].
+  specialize (pf p).
+  gen ρ.
+  induction (c p).
+  - reflexivity.
+  - intros ρ.
+    simpl.
+    dependent destruction pf.
+    simpl.
+    unfold compose_super, super.
+    rewrite Nat.add_sub.
+    rewrite H0 by auto.
+    unfold denote_u_db_box.
+    simpl.
+    remember (denote_u_db_circuit (size_wtype W) (hoas_to_db Γ (c0 p0))) as A.
+    Msimpl.
+    
